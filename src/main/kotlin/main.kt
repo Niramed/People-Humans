62e94b3336567a5e094a641aadfac54f1ca60620
Niramed <samett@bk.ru>
2022-07-01 18:00:47 +0300

    final

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main () {
+    val likes = 31
+    if (likes % 10 == 1 && likes % 100 !== 11) {
+        println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
+    }
+}
